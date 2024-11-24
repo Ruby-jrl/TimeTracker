0de55b2cc40ef63066198eb585ac10af0b17249d
Ruby-jrl <ruby.liu.sv@gmail.com>
2024-11-24 23:17:58 +0000

    minor fix - alignment, dismiss keyboard toolbar, app icon

--- TaskTracker/Views/TaskRowView.swift
+++ TaskTracker/Views/TaskRowView.swift
@@ -13,55 +13,8 @@ struct TaskRowView: View {
     @State private var hoursToAdd = ""
 
     var body: some View {
-//        VStack(alignment: .leading) {
-//            Text(task.name)
-//                .font(.headline)
-//            
-//            // Show the task goal and hours spent
-//            HStack{
-//                Text("Spent: \(task.hoursSpent, specifier: "%.1f") hours")
-//                    .font(.subheadline)
-//                Spacer()
-//                Text("Goal: \(task.goal, specifier: "%.1f") hours")
-//                    .font(.subheadline)
-//            }
-//            
-//            // Progress bar for the task
-//            ProgressView(value: task.hoursSpent, total: task.goal)
-//                .progressViewStyle(LinearProgressViewStyle())
-//                .tint(task.hoursSpent <= task.goal ? .blue : .green)
-//                .padding(1)
-//
-//            // Section to add more hours to the task
-//            HStack {
-//                TextField("Hours to add", text: $hoursToAdd)
-//                    .keyboardType(.decimalPad)
-//                    .textFieldStyle(RoundedBorderTextFieldStyle())
-//                Button(action: {
-//                    if let hours = Double(hoursToAdd) {
-//                        taskVM.updateHours(task: task, hours: hours)
-//                        hoursToAdd = ""
-//                    }
-//                }) {
-//                    Text("Add Hours")
-//                }
-//                .disabled(hoursToAdd.isEmpty)
-//            }
-//            .padding(1)
-//            
-//            // Delete button
-//            Button(action: {
-//                taskVM.removeTask(task: task)  // Call the removeTask method
-//            }) {
-//                Text("Delete Task")
-//                    .foregroundColor(.red)
-//            }
-//            .padding(1)
-//            .buttonStyle(PlainButtonStyle()) // default button style can sometimes make the entire row (or surrounding areas) seem tappable or interactive
-//        }
-//        .padding(2)
         
-        VStack {
+        VStack(alignment: .leading) {
             Text(task.name)
                 .font(.headline)
                 .padding(2)
@@ -76,13 +29,22 @@ struct TaskRowView: View {
             
             ProgressView(value: task.hoursSpent, total: task.goal)
                 .progressViewStyle(LinearProgressViewStyle())
-                .tint(task.hoursSpent <= task.goal ? .blue : .green)
+                .tint(task.hoursSpent < task.goal ? .blue : .green)
                 .padding(1)
             
             HStack {
                 TextField("Hours to add", text: $hoursToAdd)
                     .keyboardType(.decimalPad)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Spacer() // Push the button to the right
+                            Button("Done") {
+                                hideKeyboard() // Dismiss the keyboard
+                            }
+                        }
+                    }
+                    // works and seems to add toolbar for all pad
                 Button(action: {
                     if let hours = Double(hoursToAdd) {
                         taskVM.updateHours(task: task, hours: hours)
@@ -114,5 +76,9 @@ struct TaskRowView: View {
 //            }
 //        }
         
+    }
+    
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
